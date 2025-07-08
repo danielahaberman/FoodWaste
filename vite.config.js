6fee3f7ebf04c4eb6a43e96bd3b69c867126a023
Nicholas Aponte <nicholas@limosys.com>
2025-07-07 20:48:36 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-   server: {
-    port: 3000
-  }
-})
+export default {
+  base: './',
+};
